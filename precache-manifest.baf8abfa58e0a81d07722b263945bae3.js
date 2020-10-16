834d4f2b26f0d105ae73e617f28c28f5da11e714
Michael Tan <tan00060@algonquinlive.com>
2020-10-16 12:45:50 -0400

    Updates

---
+++ precache-manifest.baf8abfa58e0a81d07722b263945bae3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a1c2f7f70b8859810dce33bf0a0783",
+    "url": "/deploy-test/index.html"
+  },
+  {
+    "revision": "e5c458d4d6c4c110bd70",
+    "url": "/deploy-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2343717d443183e4804d",
+    "url": "/deploy-test/static/js/2.3eeb2579.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/deploy-test/static/js/2.3eeb2579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5c458d4d6c4c110bd70",
+    "url": "/deploy-test/static/js/main.9dd6dfe3.chunk.js"
+  },
+  {
+    "revision": "8b5a7099889fc3b8cc02",
+    "url": "/deploy-test/static/js/runtime-main.6b56c7a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
